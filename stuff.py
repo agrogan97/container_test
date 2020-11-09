824f994b4841ad2c60b30d32fd1e47e326b7751f
Alex Grogan <agrogan097@gmail.com>
2020-11-09 20:10:11 +0000

    Test changes

--- stuff.py
+++ stuff.py
@@ -1,4 +1,10 @@
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-producer.send('sample', b'Hello, World!')
-producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
+import time
+# producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-1:9092')
+# producer.send('sample', b'Hello, World!')
+# producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
+a = 0
+while True:
+    print("Hello - %d" % (a))
+    a = a + 1
+    time.sleep(2)
