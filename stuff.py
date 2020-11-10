563fcad79fbd574261a372b0d23f8b8e9f2b428b
Alex Grogan <agrogan097@gmail.com>
2020-11-10 09:16:51 +0000

    Added try-except

--- stuff.py
+++ stuff.py
@@ -2,6 +2,10 @@ from kafka import KafkaProducer
 import time
 a = 0
 while True:
-    producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092')
-    producer.send('sample', b'Hello, World!')
-    producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
+    try:
+        producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092')
+        producer.send('sample', b'Hello, World!')
+        producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
+        print("Success")
+    except:
+        print("Failure")
