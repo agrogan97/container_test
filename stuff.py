fa5096e1ca630c9ccc5c6833d1c84e14fb88c458
Alex Grogan <agrogan097@gmail.com>
2020-11-10 09:08:53 +0000

    Kafka producer loop

--- stuff.py
+++ stuff.py
@@ -1,10 +1,7 @@
 from kafka import KafkaProducer
 import time
-# producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-1:9092')
-# producer.send('sample', b'Hello, World!')
-# producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
 a = 0
 while True:
-    print("Hello - %d" % (a))
-    a = a + 1
-    time.sleep(2)
+    producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092')
+    producer.send('sample', b'Hello, World!')
+    producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
